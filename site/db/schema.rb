4469485bc2faedd45507951a9a458fbccd97ca1f
tussharsingh13 <tussharsingh13@gmail.com>
2015-04-02 23:34:32 +0530

    Admin mode added

--- site/db/schema.rb
+++ site/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150402123036) do
+ActiveRecord::Schema.define(version: 20150402173435) do
 
   create_table "contests", force: :cascade do |t|
     t.string   "name"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20150402123036) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
